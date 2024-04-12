489058bdc041f28dae2becd2150dc0f63dea2223
SandraVLP <aleksanvp@yandex.ru>
2024-04-12 14:33:48 +0300

    начало вертски

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,7 +14,23 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number,
+    user: UserType,
+    message: MessagePropsType
+}
+
+export type UserType = {
+avatar: string,
+name: string
+
+}
+
+export type MessagePropsType = {
+text: string,
+time: string
+
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
